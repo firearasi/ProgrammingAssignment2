af20be2a2c4332a1916664e03bbcec511c162ae2
程言(CHENG Yan) <chengyanslnc@gmail.com>
2014-10-26 00:10:31 +0800

    Update cachematrix.R
    Rprog-08 assignment for week 3 by firearasi

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Basically the clone of makeVector and cachemean
+## with mean replaced by inverse
 
-## Write a short comment describing this function
+## Making a "Cached version" of a matrix that can store its inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Calculate and return,the inverse of a "cached" matrix if the inverse is not calculated yet
+## Otherwise return the stored value
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
